4aa937fc6b674cabfaa35cb0d929b406b5193aea
Victor Rentea <victorrentea@gmail.com>
2019-09-23 12:29:37 +0300

    (1 to 10).map. cat de scurt e codul

--- src/main/scala/ch7/MultiplicationTable.scala
+++ src/main/scala/ch7/MultiplicationTable.scala
@@ -2,15 +2,11 @@ package ch7
 
 object MultiplicationTable extends App {
   def printMultiTable(): String = {
-    def generateCell(i: Int, j: Int) = {
+    def generateCell(i: Int, j: Int): String = {
       val prod = (i * j).toString
       " " * (4 - prod.length) + prod
     }
-
-    def generateLine(i: Int):String = {
-      val cells = for (j <- 1 to 10) yield generateCell(i, j)
-      cells.mkString("")
-    }
+    def generateLine(i: Int):String = (1 to 10).map(generateCell(i, _)).mkString("")
 
     val lines = for (i <- 1 to 10) yield generateLine(i)
     lines.mkString("\n")
