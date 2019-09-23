bb89633fbdbcb172caffe40f1a2fbce37577be8e
Victor Rentea <victorrentea@gmail.com>
2019-09-23 12:11:39 +0300

    Map

--- src/main/scala/ch1intro/FilePlay.scala
+++
@@ -1,9 +0,0 @@
-package ch1intro
-
-import scala.io.Source
-
-object FilePlay extends App {
-  val lines = Source.fromFile("build.sbt").getLines().toList
-
-  // TODO print lines with length before
-}
--- src/main/scala/ch1intro/MapPlay.scala
+++ src/main/scala/ch1intro/MapPlay.scala
@@ -6,6 +6,15 @@ object MapPlay extends App {
 
   // immutable or mutable ?
 
-  val map = Map(1.->("one"), 2 -> "two")
+  val map = mutable.Map(1.->("one"), 2 -> "two")
 
+  // map.put(3,"three")
+  map += 3 -> "three"
+  println(s"map: $map")
+
+  var immap = Map(1.->("one"), 2 -> "two")
+
+  // map.put(3,"three")
+  immap += 3 -> "three"
+  println(s"immap: $immap")
 }
