5ee153b70662776c48bbdd5d309060a8fcb9b3aa
Victor Rentea <victorrentea@gmail.com>
2019-09-23 16:51:14 +0300

    "stream API" in Scala vs For. parca castiga..

--- src/main/scala/ch7/Erori.scala
+++ src/main/scala/ch7/Erori.scala
@@ -5,21 +5,27 @@ import java.sql.SQLException
 
 object Erori extends  App{
 
-  m()
+  println(m())
 
-  def m(): Unit = {
+  def m(): Int = {
     try {
-      altaProasta(-1);
+      altaProasta(0)
+      1
     } catch {
       case e @ (_:IOException|_:SQLException) =>
         println("Shaorma: " + e)
+        -1
       case e:Exception => println("Cartofi prajiti: " + e)
+        -1
       case _ => println("Duc gunoiul ca altceva nu merit in viata")
+        -1
+    } finally {
+      return 2 // puscarie
     }
   }
   def altaProasta(i:Int): Unit  = {
     if (i<0) throw new IOException("Ntz")
     if (i>0) throw new Exception("Ntz")
-    if (i==0) throw new OutOfMemoryError();
+//    if (i==0) throw new OutOfMemoryError();
   }
 }
--- src/main/scala/ch7/ForUnleashed.scala
+++ src/main/scala/ch7/ForUnleashed.scala
@@ -9,13 +9,30 @@ object ForUnleashed extends App {
   println(totScala("""C:\workspace\scala\src\main\scala\ch1intro"""))
 
   def totScala(path:String):String = {
-    val listBuffer = for (file <- new File(path).listFiles() // .stream()
-                          if file.getName.endsWith(".scala"); //.filter()
-                          lines = fileLines(file); //.map()
-                          line <- lines //.flatMap
-         ) yield line.toUpperCase()
-    listBuffer.mkString("\n")
+//    val listBuffer = for (file <- new File(path).listFiles() // .stream()
+//                          if file.getName.endsWith(".scala"); //.filter()
+//                          lines = fileLines(file); //.map()
+//                          line <- lines //.flatMap
+//         ) yield line.toUpperCase()
+//    listBuffer.mkString("\n")
+    new File(path).listFiles()
+        .filter(_.getName.endsWith(".scala"))
+        .flatMap(fileLines(_))
+        .map(_.toUpperCase())
+        .mkString("\n")
+
   }
+
+  // pt Madalina
+//  new File(path).listFiles()
+//    .filter(f=> {
+//      println(s"filter $f" )
+//      f.getName.endsWith(".scala")
+//    })
+//    .filter(f=> {
+//      println(s"filter2 $f" )
+//      f.getName.endsWith(".scala")
+//    })
 
   private def fileLines(file: File): List[String] = {
     Source.fromFile(file).getLines().toList
