edcf88369ebbdc6c44d8b17de3dfe0896cce9928
Victor Rentea <victorrentea@gmail.com>
2019-09-23 12:30:52 +0300

    better names

--- src/main/scala/ch7/MultiplicationTable.scala
+++ src/main/scala/ch7/MultiplicationTable.scala
@@ -4,7 +4,8 @@ object MultiplicationTable extends App {
   def printMultiTable(): String = {
     def generateCell(i: Int, j: Int): String = {
       val prod = (i * j).toString
-      " " * (4 - prod.length) + prod
+      val padding = " " * (4 - prod.length)
+      padding + prod
     }
     def generateLine(i: Int):String = (1 to 10).map(generateCell(i, _)).mkString("")
 
