e6226e440715586cb0bde5cab9828da0c386a7cd
Victor Rentea <victorrentea@gmail.com>
2019-09-23 10:21:37 +0300

    Prima noastra clasa

--- src/main/scala/ch1intro/First.scala
+++ src/main/scala/ch1intro/First.scala
@@ -1,12 +1,35 @@
 package ch1intro
 
-object First extends App {
-  println("Halo")
+//object First {
+//  println("Halo")
+//
+//  // TODO print all args
+//
+//  // TODO sum
+//
+//  // TODO max
+//
+//}
 
-  // TODO print all args
+//public static void main()
 
-  // TODO sum
 
-  // TODO max
+class First {
+  // totul by default e public in scala
+  // vreau sa va fortati sa scrieti functii fara { adica o singura expresie = programare FUNCTIONALA
+  def sum(a: Int, b: Int): Int = a + b
+
+  def max(a: Int, b: Int): Int = if (a>b) a else b
+}
+object First { //~ contine toatele metodele STATICE ale unei clase First
+  def main (args: Array[String] ): Unit = {
+    println("Hello Scala !")
+    println(new First().sum(1,2))
+    println(new First().max(1,2))
+
+    for (a <- args) {
+      println(a.toUpperCase())
+    }
+  }
 
 }
