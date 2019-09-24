c8e9d45f82a98add7538f813cbc0cf42f059ba27
Victor Rentea <victorrentea@gmail.com>
2019-09-24 16:10:01 +0300

    visibility

--- src/main/scala/ch13/Chapter13_Visibility.scala
+++ src/main/scala/ch13/Chapter13_Visibility.scala
@@ -1,21 +1,29 @@
+
 package bobsrockets
 package navigation {
-  private[bobsrockets] class Navigator { // toti de SUB sau DIN pachetul indicat VAD inca
-    (new LegOfJourney).distance
+
+  private[bobsrockets] class Navigator { // toti de SUB sau DIN pachetul indicat VAD inca. din afara lui, nu
     protected def useStarChart() = { }
     class LegOfJourney { // class nested de instanta
       private[Navigator] val distance = 100 // e vizibila in sus pana la (inclusiv) Navigator
     }
-    private[this] var speed = 200 // e vizibil DOAR din instanta curenta
+    private[this] var speed = 200 // e vizibil DOAR din instanta curenta, e mai restrictiv ca "private"
+
+    def egal(n:Navigator) ={
+//      n.speed nu merge
+    }
   }
   class AltaDinAcelasiPachet {
     // (new Navigator).useStarChart // spre deosebire de Java, protected NU e visibil din acelasi pachet
   }
 }
 package launch { // este de fapt bobsrockets.launch
-  import bobsrockets.navigation._
   object Vehicle {
+    import bobsrockets.navigation._
     val guide = new Navigator
+    def m():Unit = {
+      new Navigator
+    }
   }
 }
 class ClasaDirectInBobsRockets {
@@ -27,4 +35,14 @@ class ClasaDirectInBobsRockets {
 
 object Chapter13_Visibility {
 
+}
+
+
+// javra : class X { class Y {}}
+class X {
+  class Y
+}
+// javra : class X1 { static class Y1 {}}
+object X1 {
+  class Y1
 }
