68c38c28e5398de78c0e76d54586e846a4a4e6ac
Victor Rentea <victorrentea@gmail.com>
2019-09-25 11:26:57 +0300

    +- covariance generice

--- src/main/scala/ch19/OptimQueue.scala
+++ src/main/scala/ch19/OptimQueue.scala
@@ -1,13 +1,13 @@
 package ch19
 
-class OptimQueue {
-  var incoming:List[Int] = Nil
-  var outgoing:List[Int] = Nil
+class OptimQueue[T] {
+  var incoming:List[T] = Nil
+  var outgoing:List[T] = Nil
 
-  def push(e: Int): Unit = {
+  def push(e: T): Unit = {
     incoming = e :: incoming
   }
-  def pull(): Int = {
+  def pull(): T = {
     reverse()
     val rez = outgoing.head
     outgoing = outgoing.tail
@@ -24,10 +24,49 @@ class OptimQueue {
 
 
 object OptimQueuePlay extends App {
-  val q = new OptimQueue
+  val q = new OptimQueue[Int]
+//  val q2: OptimQueue[Any] = q
   q.push(1)
   q.push(2)
 
   println(q.pull())
   println(q.pull())
+
+  def m(inturi: Supplier[Int]):Unit = {
+    val i : Int = inturi.get()
+    val orice: Supplier[Any] = inturi
+    val a: Any = orice.get()
+  }
+
+  def m(cell:Cell[String]):Unit = {
+    val anyCell:Cell[Any] = cell
+    anyCell.set(1)
+    cell.get
+
+//    List<String> stringList;
+//    List l = stringList;//
+//    l.add(1);
+  }
+
+  def gunoier(g: Gunoi[Any]) = {
+    val s:String = "a"
+    val a:Any = s
+
+    val gunoiDeString: Gunoi[String] = g // deci g e un subtip de Gunoi[String]
+  }
+}
+abstract class Supplier[+T] { // covariance:
+  // daca B extends A atunci si Supplier[B] extends Supplier[A]
+  // merg in aceeasi directie
+  def get(): T
+}
+abstract class Gunoi[-T] { // contravariance:
+  // daca B extends A atunci si Gunoi[A] extends Gunoi[B]
+  // merg in directii diferite
+  def aruncaLaGunoi(s: T)
+}
+
+abstract class Cell[+T] {
+  def set(t: T)
+  def get:T
 }
