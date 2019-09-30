0808424ed3fb3d2367f5f96e0488c9b74f916c0a
Victor Rentea <victorrentea@gmail.com>
2019-09-30 10:57:42 +0300

    inainte de exercitiul de colectii

--- src/main/scala/old/CollectionsPlay.scala
+++ src/main/scala/old/CollectionsPlay.scala
@@ -19,6 +19,22 @@ object CollectionsPlay extends App {
     Transaction(alan, 2012, 950)
   )
 
+//  println(tra)
+
+//  all_2011_transactions_sorted_by_value_desc
+
+
+
+//  unique_cities_of_the_traders
+//  traders_from_Cabridge_sorted_by_name
+//  names_of_all_traders_sorted_joined
+//  are_traders_in_Milano
+//  sum_of_values_of_transactions_from_Cambridge_traders
+//  max_transaction_value
+//  transaction_with_smallest_value
+//  fibonacci_first_10
+//  a_transaction_from_2012
+//  uniqueCharactersOfManyWords
 
 
 
