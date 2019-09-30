ac3f20e9836f2ae043d3e4560d3cbebd9679d02b
Victor Rentea <victorrentea@gmail.com>
2019-09-30 12:22:06 +0300

    setteri in stilul Scala

--- src/main/scala/ch26/GetterSetter.scala
+++ src/main/scala/ch26/GetterSetter.scala
@@ -4,9 +4,9 @@ object GetterSetter extends App {
 
   val contor = new Contor()
   println(contor.v) // 0
-  contor.setV(10)
+  contor.v = 10
   println(contor.v)
-  contor.setV(-10)
+  contor.v = -10
 //  contor.v = -11
   println(contor.v)
 }
@@ -14,7 +14,7 @@ object GetterSetter extends App {
 // negativa.
 //daca se da negativa sa ramana la vechea valoare
 class Contor(private var _v: Int = 0) {
-  def setV(value: Int):Unit = if (value >= 0) _v = value // if e folosit ca statement nu ca expresie
+  def v_=(value: Int):Unit = if (value >= 0) _v = value // if e folosit ca statement nu ca expresie
   def v: Int = _v // by default incercam sa facem totul val.
   // vom reusi daca clasele noastre sunt imutabile (sau case class)
   // Dar aici trebuie def pt ca se modifica starea interna a clasei
