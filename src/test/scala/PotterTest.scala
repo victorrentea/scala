c84f8922ba2b8579e099e8fb2fbb5341e79057b0
Victor Rentea <victorrentea@gmail.com>
2019-10-01 14:02:37 +0300

    fix tests

--- src/test/scala/PotterTest.scala
+++ src/test/scala/PotterTest.scala
@@ -17,7 +17,7 @@ class PotterTest extends FunSuite {
     assert(price(Array(1->1, 2->1, 3->1, 4->1)) == 4 * 8 * .80)
   }
   test("1,2,3,4,5") {
-    assert(price(Array(1->1, 2->1, 3->1, 4->1, 5->1)) == 5 * 8 * .25)
+    assert(price(Array(1->1, 2->1, 3->1, 4->1, 5->1)) == 5 * 8 * .75)
   }
   test("tot") {
     assert(price(Array(1->2, 2->2, 3->2, 4->1, 5->1)) == 51.2)
